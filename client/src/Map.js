7075d975ef14f70f04338c09a97fe1c68c931cfd
Graham Tompkins <gtompkins1993@gmail.com>
2021-08-02 20:44:54 -0300

    Fixed polylines to follow shortest path around globe

--- client/src/Map.js
+++ client/src/Map.js
@@ -11,71 +11,85 @@ import {
 
 const mapContainerStyle = {
   width: "800px",
-  height: "400px",
+  height: "500px",
 };
 const libraries = ["geometry"];
-const zoom = 2;
+// const zoom = 2;
 
 // const center = {lat: 0, lng: 0};
 
-const RenderMap = ({
-  clickedLat,
-  clickedLng,
-  mapClickHandler,
-  midpoint,
-  nyc,
-  center,
-  line,
-  midpointLat,
-  midpointLng,
-}) => {
-  if (midpoint) {
-    console.log("midpoint", midpoint.lat(), midpoint.lng());
-  }
+// const RenderMap = ({
+//   clickedLat,
+//   clickedLng,
+//   mapClickHandler,
+//   midpoint,
+//   nyc,
+//   center,
+//   line,
+//   midpointLat,
+//   midpointLng,
+// }) => {
+//   if (midpoint) {
+//     console.log("midpoint", midpoint.lat(), midpoint.lng());
+//   }
 
-  // const {center, recenter} = useContext(MapContext);
-  const lineOptions = {
-    strokeColor: "#FF0000",
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: "#FF0000",
-    fillOpacity: 0.35,
-    clickable: false,
-    draggable: false,
-    editable: false,
-    visible: true,
-    radius: 30000,
-  };
+//   // const {center, recenter} = useContext(MapContext);
+//   const lineOptions = {
+//     strokeColor: "#FF0000",
+//     strokeOpacity: 0.8,
+//     strokeWeight: 2,
+//     fillColor: "#FF0000",
+//     fillOpacity: 0.35,
+//     clickable: false,
+//     draggable: false,
+//     editable: false,
+//     visible: true,
+//     radius: 30000,
+//   };
 
-  return (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      zoom={zoom}
-      center={center}
-      onClick={(ev) => {
-        mapClickHandler(ev);
-      }}
-      // onLoad={onLoad}
-    >
-      {clickedLat && clickedLng && (
-        <>
-          <Marker position={{lat: clickedLat, lng: clickedLng}} />
-          {midpoint && (
-            <Marker
-              position={{lat: midpoint.lat(), lng: midpoint.lng()}}
-            ></Marker>
-          )}
-          {nyc && <Marker position={{lat: nyc.lat(), lng: nyc.lng()}}></Marker>}
-        </>
-      )}
-      {line && (
-        <Polyline
-          path={[{lat: clickedLat, lng: clickedLng}, nyc]}
-          options={lineOptions}
-        ></Polyline>
-      )}
-    </GoogleMap>
-  );
+//   return (
+//     <GoogleMap
+//       mapContainerStyle={mapContainerStyle}
+//       zoom={zoom}
+//       center={center}
+//       onClick={(ev) => {
+//         mapClickHandler(ev);
+//       }}
+//       // onLoad={onLoad}
+//     >
+//       {clickedLat && clickedLng && (
+//         <>
+//           <Marker position={{lat: clickedLat, lng: clickedLng}} />
+//           {midpoint && (
+//             <Marker
+//               position={{lat: midpoint.lat(), lng: midpoint.lng()}}
+//             ></Marker>
+//           )}
+//           {nyc && <Marker position={{lat: nyc.lat(), lng: nyc.lng()}}></Marker>}
+//         </>
+//       )}
+//       {line && (
+//         <Polyline
+//           path={[{lat: clickedLat, midpoint, lng: clickedLng}, nyc]}
+//           options={lineOptions}
+//         ></Polyline>
+//       )}
+//     </GoogleMap>
+//   );
+// };
+
+const lineOptions = {
+  strokeColor: "#FF0000",
+  strokeOpacity: 0.8,
+  strokeWeight: 2,
+  fillColor: "#FF0000",
+  fillOpacity: 0.35,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  radius: 30000,
+  geodesic: false,
 };
 
 const Map = () => {
@@ -83,10 +97,11 @@ const Map = () => {
   const [clickedLng, setClickedLng] = useState(null);
   const [midpointLat, setMidpointLat] = useState(null);
   const [midpointLng, setMidpointLng] = useState(null);
+  const [distance, setDistance] = useState(null);
   const [midpoint, setMidpoint] = useState(null);
   const [line, setLine] = useState(false);
   const path = [];
-  const {center, recenter} = useContext(MapContext);
+  const {center, recenter, zoom, setZoom} = useContext(MapContext);
 
   // const drawLine = (clickSpot, answer) => {
   //   // const Line = new google.map.PolyLine({
@@ -120,7 +135,7 @@ const Map = () => {
   // let clickedLng = null;
   // let clickedLat = null
   let clickSpot = null;
-  let distanceTest = null;
+  // let distance = null;
   // let midpointLat = null;
   // let midpointLng = null;
   // let midpoint = null;
@@ -134,30 +149,26 @@ const Map = () => {
       console.log(ev.latLng.lng(), "lng", ev.latLng.lat(), "lat");
       setClickedLng(ev.latLng.lng());
       setClickedLat(ev.latLng.lat());
-      distanceTest = google.maps.geometry.spherical.computeDistanceBetween(
-        nyc,
-        clickSpot
+      setDistance(
+        google.maps.geometry.spherical.computeDistanceBetween(nyc, clickSpot)
       );
       let midpointLat = (ev.latLng.lat() + 40.715) / 2;
       let midpointLng = (ev.latLng.lng() - 74.002) / 2;
+      if (Math.abs(midpointLng - ev.latLng.lng()) > 90) {
+        midpointLng = midpointLng + 180;
+      }
       setMidpoint(new google.maps.LatLng(midpointLat, midpointLng));
 
       console.log("midpoint", midpoint);
-      console.log(distanceTest);
-    }, 50);
+      console.log(distance);
+    }, 350);
   };
-
-  console.log("london", london);
-  //   if (google) {
-  console.log(google);
-  let distance = google.maps.geometry.spherical.computeDistanceBetween(
-    nyc,
-    london
-  );
+  console.log("distance", distance);
+  console.log("zoom", zoom);
 
   return (
     <div>
-      <RenderMap
+      {/* <RenderMap
         clickedLat={clickedLat}
         clickedLng={clickedLng}
         mapClickHandler={mapClickHandler}
@@ -167,10 +178,39 @@ const Map = () => {
         line={line}
         // midpointLng={midpointLng}
         // midpointLat={midpointLat}
-      />
+      /> */}
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={zoom}
+        center={center}
+        onClick={(ev) => {
+          mapClickHandler(ev);
+        }}
+        // onLoad={onLoad}
+      >
+        {clickedLat && clickedLng && (
+          <>
+            <Marker position={{lat: clickedLat, lng: clickedLng}} />
+            {midpoint && (
+              <Marker
+                position={{lat: midpoint.lat(), lng: midpoint.lng()}}
+              ></Marker>
+            )}
+            {nyc && (
+              <Marker position={{lat: nyc.lat(), lng: nyc.lng()}}></Marker>
+            )}
+          </>
+        )}
+        {line && (
+          <Polyline
+            path={[{lat: clickedLat, lng: clickedLng}, midpoint, nyc]}
+            options={lineOptions}
+          ></Polyline>
+        )}
+      </GoogleMap>
       <button
         onClick={() => {
-          recenter(midpoint.lat(), midpoint.lng());
+          recenter(midpoint.lat(), midpoint.lng(), distance);
           setLine(true);
         }}
       >
--- client/src/MapContext.js
+++ client/src/MapContext.js
@@ -5,13 +5,34 @@ export const MapContext = createContext(null);
 
 export const MapContextProvider = ({children}) => {
   const [center, setCenter] = useState({lat: 0, lng: 0});
-
-  const recenter = (lat, lng) => {
+  const [zoom, setZoom] = useState(1);
+  const recenter = (lat, lng, distance) => {
     setCenter({lat, lng});
+    if (distance > 4000000) {
+      setZoom(2);
+    } else if (distance > 2100000) {
+      setZoom(3);
+    } else if (distance > 1000000) {
+      setZoom(4);
+    } else if (distance > 500000) {
+      setZoom(5);
+    } else if (distance > 200000) {
+      setZoom(6);
+    } else if (distance > 100000) {
+      setZoom(7);
+    } else if (distance > 50000) {
+      setZoom(8);
+    } else if (distance > 20000) {
+      setZoom(9);
+    } else if (distance > 5000) {
+      setZoom(10);
+    } else if (distance < 1000) {
+      setZoom(11);
+    }
   };
 
   return (
-    <MapContext.Provider value={{center, recenter}}>
+    <MapContext.Provider value={{center, recenter, zoom, setZoom}}>
       {children}
     </MapContext.Provider>
   );
