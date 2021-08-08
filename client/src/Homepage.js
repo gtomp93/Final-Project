55dd051b3cc27bc5225e66ccab1c4865531bb2aa
Graham Tompkins <gtompkins1993@gmail.com>
2021-08-08 13:34:24 -0300

    Load all games on homepage

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -5,11 +5,20 @@ import styled from "styled-components";
 const Homepage = () => {
   const [games, setGames] = useState(null);
 
-  fetch("/getGames")
-    .then((res) => res.json())
-    .then((res) => {
-      console.log("this thing", res);
-    });
+  useEffect(() => {
+    fetch("/getGames")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("this thing", res);
+        setGames(res.result);
+      });
+  }, []);
+
+  console.log(games);
+
+  if (!games) {
+    return "loading";
+  }
 
   return (
     <div style={{marginLeft: "20px"}}>
@@ -23,9 +32,26 @@ const Homepage = () => {
       <div style={{width: "100px", height: "50px", border: "solid black 1px"}}>
         <Link to={"/gameOptions/worldTour"}>Cities</Link>
       </div>
-      <Link to={"/CreateMap"}>Create Map</Link>
+      <Link to={"/CreateMapForm"}>Create Map</Link>
+      {games.map((game) => {
+        console.log(game.pic);
+        return (
+          <GameContainer>
+            <div>{game.name}</div>
+            <GamePic src={game.pic}></GamePic>
+            <div>{game.description}</div>
+          </GameContainer>
+        );
+      })}
     </div>
   );
 };
+
+const GameContainer = styled.div``;
+
+const GamePic = styled.img`
+  width: 50px;
+  display: block;
+`;
 
 export default Homepage;
